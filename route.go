8eee5746c5daedc04180bc673c4471fb7061aa2a
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2025-05-20 22:48:27 +0100

    Impl GetByCurrDest

--- route.go
+++ route.go
@@ -39,25 +39,49 @@ type (
 		GetByCurrDest(current, dest Id) (Route, StoreError)
 	}
 
-	routeStoreLocal map[Id]Route
+	routeStoreLocal struct {
+		routes map[Id]Route
+		router Router
+	}
 
 	RouteStoreErrorCode int
 )
 
-func (rsl routeStoreLocal) GetByCurrDest(curr Id, dest Id) (Route, StoreError) {
-	routes, err := rsl.All()
-	if err != nil {
-		return Route{}, err
-	}
-	panic(routes)
+func (e IdDoesntExist) RouterErrorCode() RouterErrorCode {
+	return RouterErrIdDoesntExist
+}
 
+func (rsl routeStoreLocal) GetByCurrDest(curr Id, dest Id) (Route, RouterError) {
+	routes, stErr := rsl.All()
+	if stErr != nil {
+		switch stErr.StoreErrorCode() {
+		case StoreErrorIdDoesntExist:
+			return Route{}, idDoesntExist()
+		case StoreErrorInternalError:
+			return Route{}, internalServerError{stErr}
+		}
+	}
+
+	for _, r := range routes {
+		if r.CurrentId == curr && r.DestId == dest {
+			return r, nil
+		}
+	}
+
+	route, rtErr := rsl.router.Route(curr, dest)
+
+	if rtErr != nil {
+		return Route{}, rtErr
+	}
+
+	return route, nil
 }
 
 func (rsl routeStoreLocal) All() (map[Id]Route, StoreError) {
-	return rsl, nil
+	return rsl.routes, nil
 }
 func (rsl routeStoreLocal) GetById(id Id) (Route, StoreError) {
-	route, found := rsl[id]
+	route, found := rsl.routes[id]
 
 	if !found {
 		return Route{}, IdDoesntExist{}
